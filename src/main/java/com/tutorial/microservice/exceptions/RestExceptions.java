604fba983a48dc4e2708cdaa309729d459bda10f
ervitis <vitomarti@gmail.com>
2019-05-05 20:24:11 +0200

    feat: handle exceptions

--- src/main/java/com/tutorial/microservice/exceptions/RestExceptions.java
+++ src/main/java/com/tutorial/microservice/exceptions/RestExceptions.java
@@ -29,7 +29,7 @@ public class RestExceptions extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({NullPointerException.class, IllegalArgumentException.class, IllegalStateException.class})
-    /*500*/ public ResponseEntity<Object> handleInternal(final RuntimeException ex, final WebRequest request) {
+    public ResponseEntity<Object> handleInternal(final RuntimeException ex, final WebRequest request) {
         logger.error("500 Status Code", ex);
         final String bodyOfResponse = "This should be application specific";
         return handleExceptionInternal(ex, new ApiError("Internal server error"), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
