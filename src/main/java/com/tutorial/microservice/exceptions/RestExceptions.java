0540237805e28c8b50eb3ea6c977857163fc06d1
ervitis <vitomarti@gmail.com>
2019-05-05 21:35:13 +0200

    fix: deleted unused line

--- src/main/java/com/tutorial/microservice/exceptions/RestExceptions.java
+++ src/main/java/com/tutorial/microservice/exceptions/RestExceptions.java
@@ -31,7 +31,6 @@ public class RestExceptions extends ResponseEntityExceptionHandler {
     @ExceptionHandler({NullPointerException.class, IllegalArgumentException.class, IllegalStateException.class})
     public ResponseEntity<Object> handleInternal(final RuntimeException ex, final WebRequest request) {
         logger.error("500 Status Code", ex);
-        final String bodyOfResponse = "This should be application specific";
         return handleExceptionInternal(ex, new ApiError("Internal server error"), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
